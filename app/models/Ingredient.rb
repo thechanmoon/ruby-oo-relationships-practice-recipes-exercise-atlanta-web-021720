d144bb5873391ce79b1cadf9089261c05d1da2f2
thechanmoon <thechanmoon@gmail.com>
2020-02-24 14:10:49 -0500

    Ingredient completed

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,16 +1,31 @@
 class Ingredient
 
+    attr_reader :name
+    
     @@all = []
     
-    def initialize()
+    def initialize(name)
+        @name = name
         @@all << self
+
     end
 
     def self.all
         @@all
     end
 
-    def most_common_allergen
+    def ingredient
+        @ingredient
+    end
     
+    # def allergys
+    #     Allergy.all.map { |allergy| allergy.ingredient.name}
+    # end
+
+    def self.most_common_allergen
+        # Allergy.all.map { |allergy| allergy.ingredient}.map{|el| el.ingredient}
+         newArray = Allergy.all.map { |allergy| allergy.ingredient.name}
+         newArray.max_by{|el|newArray.count(el)}
+        #@allergys.max_by{|el|newArray.count(el)}
     end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,15 @@
 require_relative '../config/environment.rb'
 
+
 binding.pry
+
+# u= User.new("Ian")
+# r = RecipeCard.new("02/24/20202", 0.3, u, "good recipe")
+# u.add_recipe_card("good recipe_2", "02/20/20202", 0.4)
+# u.add_recipe_card("good recipe_3", "02/20/20202", 0.5)
+# u.add_recipe_card("good recipe_4", "02/20/20202", 0.6)
+# u.declare_allergy("Soy")
+
+# i = Ingredient.new("Soy")
+# a = Allergy.new(u,i)
+# u.declare_allergy(i)
