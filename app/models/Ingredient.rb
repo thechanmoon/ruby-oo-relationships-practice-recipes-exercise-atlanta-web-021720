a45620ec0fc04329c35f2a6f4f8491dab4e74036
thechanmoon <thechanmoon@gmail.com>
2020-02-25 14:04:26 -0500

    most_common_allergen changed

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -23,9 +23,10 @@ class Ingredient
     # end
 
     def self.most_common_allergen
-        # Allergy.all.map { |allergy| allergy.ingredient}.map{|el| el.ingredient}
-         newArray = Allergy.all.map { |allergy| allergy.ingredient.name}
-         newArray.max_by{|el|newArray.count(el)}
-        #@allergys.max_by{|el|newArray.count(el)}
+        #  newArray = Allergy.all.map { |allergy| allergy.ingredient.name}
+        #  newArray.max_by{|el|newArray.count(el)}
+ 
+        newArray = Allergy.all.map { |allergy| allergy.ingredient}
+        newArray.max_by{|el|newArray.count(el)}
     end
 end
